b640345a406f784065715eb7dad80fdc19ceba82
Max Gudmundson <maxgud@gmail.com>
2018-08-27 17:27:34 -0600

    matching commit

---
+++ main.js
@@ -0,0 +1,12 @@
+$(document).ready( function() {
+    if (on.click)
+      $('#empty').show();
+    else
+      $('#empty').hide();
+
+}
+
+/*
+google how to hide a picture and how to show a picture in
+javascript
+*/
